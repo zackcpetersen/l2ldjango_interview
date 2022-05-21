2d48c54ed48f0ccc31e324939f7bcfa21f2e7d96
zackckpetersen <zackcpetersen@gmail.com>
2022-05-21 11:23:06 -0600

    adding custom error handling

--- l2l/templatetags/l2l_extras.py
+++ l2l/templatetags/l2l_extras.py
@@ -6,8 +6,13 @@ from django import template
 register = template.Library()
 
 def l2l_dt(value):
+    # Expecting datetime or str, else raise TypeError
     if isinstance(value, str):
         value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
+    elif isinstance(value, datetime):
+        pass
+    else:
+        raise TypeError(f'Unexpected type. Expected datetime or str, instead got {type(value)}')
     
     return value.strftime("%Y-%m-%d %H:%M:%S")
 
